998745f8baf0dda94d27d8b9c2d7d4fa4b516aed
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-11-30 19:16:43 +0600

    modify navbar

--- src/pages/Shared/navbar/Navbar.js
+++ src/pages/Shared/navbar/Navbar.js
@@ -54,13 +54,16 @@ const Navbar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              
             {navmenu}
             </ul>
           </div>
+          <img src={'logo.png'} alt="" className='w-10 h-10 rounded-full' />
           <Link className="btn btn-ghost normal-case text-xl">Resale Zone</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
+          
            {navmenu}
           </ul>
         </div>
