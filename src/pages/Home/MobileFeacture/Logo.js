c443676400553277bb61dcad5e8ef613a6981aea
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-12-01 19:58:27 +0600

    added company logo

--- src/pages/Home/MobileFeacture/Logo.js
+++ src/pages/Home/MobileFeacture/Logo.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <div>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 my-20 gap-5'>
 
-            <img src={visualViewport.jpg} alt="" />
+            <img src={"iphone-logo.png"} alt=""  className='w-48 h-28 rounded'/>
+            <img src={"oneplus.jpg"} alt="" className='w-48 h-28 rounded' />
+            <img src={"xiaomi.jpg"} alt="" className='w-48 h-28 rounded' />
+            <img src={"oppo.jpg"} alt="" className='w-48 h-28 rounded' />
+            <img src={"vivo.jpg"} alt="" className='w-48 h-28 rounded' />
             
         </div>
     );
